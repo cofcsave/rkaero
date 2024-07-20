e64f440ef8fdd709c038255fe990d8cc557e203a
cofcsave <cofcsave@gmail.com>
2024-07-20 15:10:34 -0500

    adding react source files

---
+++ data.js
@@ -0,0 +1,104 @@
+
+export const NAV_LINKS = [
+    {
+      copy: 'Instructions',
+      url: '#instructions',
+    },
+    {
+    copy: 'Classes',
+    url: '#classes',
+    },
+    {
+    copy: 'Rentals',
+    url: '#rentals',
+    },
+    {
+    copy: 'Travel',
+    url: '#travel',
+    }
+  ];
+
+export const CLASSES = [
+    {
+        year: 2024,
+        month: 'March',
+        classDays: '29 - Apr. 1',
+        lakeDays: 'By Referral'
+      
+    },
+    {
+        year: 2024,
+        month: 'April',
+        classDays: '26 - 29',
+        lakeDays: 'By Referral',
+    },
+    {
+        year: 2024,
+        month: 'May',
+        classDays: '17 - 20',
+        lakeDays: 'June 1, 2',
+    },
+    {
+        year: 2024,
+        month: 'June',
+        classDays: '7 - 10',
+        lakeDays: '15, 16',
+    },
+    {
+        year: 2024,
+        month: 'July',
+        classDays: 'Jun. 28 - 1',
+        lakeDays: '13, 14',
+    },
+    {
+        year: 2024,
+        month: 'July',
+        classDays: '5 -8',
+        lakeDays: '13, 14',
+    },
+    {
+        year: 2024,
+        month: 'July',
+        classDays: '26 - 29',
+        lakeDays: 'Aug. 3, 4',
+    },
+    {
+        year: 2024,
+        month: 'August',
+        classDays: '16 - 19',
+        lakeDays: '24, 25',
+    }
+];
+
+export const TRAVEL_DATES = [
+    {
+        location:'BVI Sail & Dive',
+        dates:'August 4 - 11',
+        price:'$2999 + Air',
+        soldOut: true
+    },
+    {
+        location:'St. Lucia - Martinique, Sail & Dive',
+        dates:'October 19 - 26',
+        price:'$2999 + Air',
+        soldOut: true
+    },
+    {
+        location:'St. Lucia - Martinique, Sail & Dive',
+        dates:'Oct. 28 - Nov. 4',
+        price:'$2999 + Air',
+        soldOut: true
+    },
+    {
+        location:'Cozumel',
+        dates:'Nov. 29 - Dec. 2',
+        price:'$1399 + Air',
+        soldOut: false
+    },
+    {
+        location:'Fiji Beqa Lagoon',
+        dates:'October 4 - 11 2025',
+        price:'$3599 W/Air (LAX)',
+        soldOut: false
+    }
+];
